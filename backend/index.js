73026b400d0c30214d1544ae07df733fda557e29
chathuwa <it22049872@my.sliit.lk>
2024-07-20 00:16:02 +0530

    backend initialized

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("Hello 😎");
+// packages
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// utiles
+import connectDB from "./config/db.js";
+
+// load the .env file
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.send("Hellow world");
+})
+
+app.listen(port, () => console.log(`server running on port: ${port}`))
