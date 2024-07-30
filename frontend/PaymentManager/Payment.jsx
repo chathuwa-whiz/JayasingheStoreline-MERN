7d4bd40dd872bfd89ede7913f8beaecb90b07757
Viduraz <162951228+Viduraz@users.noreply.github.com>
2024-07-30 23:23:08 +0530

    h

--- frontend/PaymentManager/Payment.jsx
+++ frontend/PaymentManager/Payment.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Payment = () => {
   return (
-    <div>Payment hii yuluwqbnsrsdgfyhgkghn</div>
+    <div>Payment hii yulbvcnv</div>
   )
-  
+
 }
 
 export default Payment
