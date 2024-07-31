ae79169de2e7842fd3c8a13a300f0063c67c6e69
chathuwa <it22049872@my.sliit.lk>
2024-08-01 01:29:15 +0530

    not showing homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Link from 'react-router-dom'
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello kohondaaaaaaa api home eka heata hdnwaaaa</h1>
+      <h1>This is Home Page</h1>
     </>
   )
 }
