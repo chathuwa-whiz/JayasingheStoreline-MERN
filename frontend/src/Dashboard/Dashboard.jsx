267de12fa7abb82614b9b78c35d2cc4536fc9017
chathuwa <it22049872@my.sliit.lk>
2024-08-01 10:13:20 +0530

    dashboard

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-<<<<<<< HEAD
-    <div>Dashboard</div>
-=======
     <div>
         <h1>This is dashboard</h1>
     </div>
->>>>>>> 827c9dbadca13efe690d3224b6fc29bc6e849507
   )
 }
