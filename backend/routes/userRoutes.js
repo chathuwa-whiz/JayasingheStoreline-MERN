a37ab4e7a98f201dd8dfa274e52006642d8296fd
AKAZJAYA <savindu096@gmail.com>
2024-08-16 16:43:26 +0530

    userroutes & userCreate3

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createUser, loginUser } from "../controllers/userControllers.js";
+import { createUser, loginUser, logoutCurrentUser } from "../controllers/userControllers.js";
+// import { authenticate, authorizeAdmin } from "../middlewares/authMidleware.js";
+
+
+
 const router = express.Router()
 
-router.route('/').post(createUser);
+router.route("/").post(createUser)
+// .get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
+router.post('/logout', logoutCurrentUser)
 
 export default router;
