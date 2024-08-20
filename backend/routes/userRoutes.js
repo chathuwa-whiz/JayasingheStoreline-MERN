4a0f69d2d824cc1325b5617c67fa4fbc83d77e5a
AKAZJAYA <savindu096@gmail.com>
2024-08-20 12:55:14 +0530

    update user & password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers } from "../controllers/userControllers.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from "../controllers/userControllers.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMidleware.js";
 
 
@@ -9,5 +9,7 @@ const router = express.Router()
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
 router.post('/logout', logoutCurrentUser)
+
+router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 export default router;
