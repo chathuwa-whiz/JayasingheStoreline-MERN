09a2dd2569b7534aee5dc570012beb9d6e4408e0
chathuwa <it22049872@my.sliit.lk>
2024-08-23 13:48:42 +0530

    orderItems came to me 😚

--- frontend/src/InventoryManagement/Stock.jsx
+++ frontend/src/InventoryManagement/Stock.jsx
@@ -6,7 +6,7 @@ export default function Stock() {
 
   // Fetch all orders
   const { data: orders } = useGetOrdersQuery();
-  console.log("Orders -> ",JSON.parse(orders[2].orderItems));
+  // console.log("Orders -> ",JSON.parse(orders[2].orderItems));
   console.log(orders.length);
   for (let index = 0; index < orders.length; index++) {
     console.log("Order Items -> ",JSON.parse(orders[index].orderItems));
