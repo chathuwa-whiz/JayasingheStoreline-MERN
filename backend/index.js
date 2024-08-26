dad099658894e0c379d5d2395545e8ae0aa987ec
nethmichamathka <chamathkanethmi11@gmail.com>
2024-08-26 15:36:16 +0530

    first merge

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,6 @@ connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());  
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
