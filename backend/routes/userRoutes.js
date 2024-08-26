8933af3f6cea0009af0f6107311e0a0fc9c34e87
chathuwa <it22049872@my.sliit.lk>
2024-08-26 22:36:11 +0530

    just add isAdmin to userController.js -> createUser -> req.body 😉

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,11 @@ import express from "express";
 import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile, deleteUserbyId, getUserById, updateUserById } from "../controllers/userControllers.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMidleware.js";
 
-
-
-const router = express.Router()
+const router = express.Router();
 
 router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post('/auth', loginUser);
-router.post('/logout', logoutCurrentUser)
+router.post('/logout', logoutCurrentUser);
 
 router.route('/profile').get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
