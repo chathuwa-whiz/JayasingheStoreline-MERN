9e4dc86909a9f0bf7de38ebb6b217ba40553a722
chathuwa-whiz <navodchathushka@gmail.com>
2024-09-23 02:05:57 +0530

    Added logout page and minor changes from settings page. no any changes in backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import UpdateProduct from "./InventoryManagement/UpdateProduct";
 import Stock from "./InventoryManagement/Stock";
 import Reports from "./InventoryManagement/Reports";
 import AddStockPage from "./InventoryManagement/AddStock";
+import LogoutPage from "./InventoryManagement/LogoutPage";
 
 //Delivery
 import Delivery from "./DeliveryManagement/delivery";
@@ -111,6 +112,7 @@ function App() {
           <Route path="reports" element={<Reports />} />
           <Route path="addstock/:_id" element={<AddStockPage />} />
           <Route path="settings" element={<SettingsPage />} />
+          <Route path="logout" element={<LogoutPage />} />
         </Route>
 
         {/*Delivery manager Routes */}
