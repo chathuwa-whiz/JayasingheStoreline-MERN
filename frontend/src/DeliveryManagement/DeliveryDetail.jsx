9ef060ae2d365d6e734d0e52796555d4a3246cce
YasithJY <it22136374@my.sliit.lk>
2024-10-13 20:52:40 +0530

    Corrections

--- frontend/src/DeliveryManagement/DeliveryDetail.jsx
+++ frontend/src/DeliveryManagement/DeliveryDetail.jsx
@@ -145,7 +145,6 @@ export default function DeliveryDetail({ onEditDelivery }) {
         <thead className="bg-gray-200 text-gray-700">
           <tr>
             <th className="border p-3 text-left">Delivery No</th>
-            <th className='border p-3 text-left'>Order No</th>
             <th className="border p-3 text-left">Delivery Item</th>
             <th className="border p-3 text-left">Name</th>
             <th className="border p-3 text-left">Contact No</th>
